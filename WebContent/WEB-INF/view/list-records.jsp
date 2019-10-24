29fece2fa443ac88ee61ace619a7b2cf4b25f019
Iryna <iren1iren2iren3@gmail.com>
2019-10-24 12:49:24 +0300

    Update those files to select and display all db data.

--- WebContent/WEB-INF/view/list-records.jsp
+++ WebContent/WEB-INF/view/list-records.jsp
@@ -1,7 +1,34 @@
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html>
 <html>
 <head><title>List Records</title></head>
 <body>
-There will be added List Records soon...
+	<div id="wrapper">
+		<div id="header">
+			<h2> Candy - Service fire detector</h2>
+		</div>
+   	</div>
+   	<div id="container">
+		<div id="context">
+			<table>
+			<tr>
+				<th>Date</th>
+				<th>Detector Name</th>
+				<th>Global Type of Reason</th> 
+				<th>Reason</th>
+			</tr>
+			<c:forEach var="tmpRecord" items ="${records}">
+			  <tr>
+				<td>${tmpRecord.date}</td>
+				<td>${tmpRecord.detectorName.name}</td>
+				<td>${tmpRecord.globalReason}</td> 
+				<td>${tmpRecord.simpleReason}</td>
+			  </tr>
+			</c:forEach>
+			</table>	
+				  
+		</div>
+   	</div>
+   			
 </body>
 </html>
--- src/iryna/bozhyk/service/controller/RecordsController.java
+++ src/iryna/bozhyk/service/controller/RecordsController.java
@@ -1,15 +1,27 @@
 package iryna.bozhyk.service.controller;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import iryna.bozhyk.service.dao.MyServiceDAO;
+import iryna.bozhyk.service.entity.MyService;
 
 @Controller
 @RequestMapping("/records")
 public class RecordsController {
 	
+	@Autowired
+	@Qualifier("myServiceDAOImpl")
+	private MyServiceDAO myServiceDAO;
+	
 	@RequestMapping("/list")
 	public String listRecords(Model theModel){
+		List <MyService> theServices = myServiceDAO.getMyServices();
+		theModel.addAttribute("records", theServices);
 		return "list-records";
 	}
 
